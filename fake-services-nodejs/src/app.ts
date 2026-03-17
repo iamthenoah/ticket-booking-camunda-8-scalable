502425369996d37b7eee71bb18c7b555c8bb7ec6
Noah G <greffnoah@gmail.com>
2026-03-17 18:15:27 +0100

    formatting

--- fake-services-nodejs/src/app.ts
+++ fake-services-nodejs/src/app.ts
@@ -8,14 +8,14 @@ import { metricsContentType, renderMetrics, startStep } from './metrics'
 require('dotenv').config()
 
 type PaymentRequestMessage = {
-  paymentRequestId: string
-  requestSentAtEpochMs: number
+	paymentRequestId: string
+	requestSentAtEpochMs: number
 }
 
 type PaymentResponseMessage = {
-  paymentRequestId: string
-  paymentConfirmationId: string
-  requestSentAtEpochMs: number
+	paymentRequestId: string
+	paymentConfirmationId: string
+	requestSentAtEpochMs: number
 }
 
 ////////////////////////////////////
@@ -25,36 +25,42 @@ const zeebeClient = new ZBClient()
 zeebeClient.createWorker('reserve-seats', reserveSeatsHandler)
 
 function reserveSeatsHandler(job: any) {
-  const observation = startStep('reserve_seats')
-  console.log('\n\n Reserve seats now...')
-  console.log(job)
+	const observation = startStep('reserve_seats')
+	console.log('\n\n Reserve seats now...')
+	console.log(job)
 
-  try {
-    if ('seats' !== job.variables.simulateBookingFailure) {
-      console.log('Successul :-)')
-      return job.complete({
-        reservationId: '1234'
-      }).then((ack: any) => {
-        observation.stop('success')
-        return ack
-      }).catch((error: unknown) => {
-        observation.stop('error')
-        throw error
-      })
-    }
+	try {
+		if ('seats' !== job.variables.simulateBookingFailure) {
+			console.log('Successul :-)')
+			return job
+				.complete({
+					reservationId: '1234'
+				})
+				.then((ack: any) => {
+					observation.stop('success')
+					return ack
+				})
+				.catch((error: unknown) => {
+					observation.stop('error')
+					throw error
+				})
+		}
 
-    console.log('ERROR: Seats could not be reserved!')
-    return job.error('ErrorSeatsNotAvailable').then((ack: any) => {
-      observation.stop('error')
-      return ack
-    }).catch((error: unknown) => {
-      observation.stop('error')
-      throw error
-    })
-  } catch (error) {
-    observation.stop('error')
-    throw error
-  }
+		console.log('ERROR: Seats could not be reserved!')
+		return job
+			.error('ErrorSeatsNotAvailable')
+			.then((ack: any) => {
+				observation.stop('error')
+				return ack
+			})
+			.catch((error: unknown) => {
+				observation.stop('error')
+				throw error
+			})
+	} catch (error) {
+		observation.stop('error')
+		throw error
+	}
 }
 
 ////////////////////////////////////
@@ -64,49 +70,49 @@ const queuePaymentRequest = 'paymentRequest'
 const queuePaymentResponse = 'paymentResponse'
 
 amqp.connect('amqp://rabbitmq', function (error0: Error | null, connection: any) {
-  if (error0) {
-    throw error0
-  }
-  connection.createChannel(function (error1: Error | null, channel: any) {
-    if (error1) {
-      throw error1
-    }
+	if (error0) {
+		throw error0
+	}
+	connection.createChannel(function (error1: Error | null, channel: any) {
+		if (error1) {
+			throw error1
+		}
 
-    channel.assertQueue(queuePaymentRequest, { durable: true })
-    channel.assertQueue(queuePaymentResponse, { durable: true })
+		channel.assertQueue(queuePaymentRequest, { durable: true })
+		channel.assertQueue(queuePaymentResponse, { durable: true })
 
-    channel.consume(
-      queuePaymentRequest,
-      function (inputMessage: any) {
-        if (!inputMessage) {
-          return
-        }
+		channel.consume(
+			queuePaymentRequest,
+			function (inputMessage: any) {
+				if (!inputMessage) {
+					return
+				}
 
-        const observation = startStep('payment_consume')
-        try {
-          const paymentRequest = JSON.parse(inputMessage.content.toString()) as PaymentRequestMessage
-          const paymentConfirmationId = uuidv4()
-          const outputMessage: PaymentResponseMessage = {
-            paymentRequestId: paymentRequest.paymentRequestId,
-            paymentConfirmationId,
-            requestSentAtEpochMs: paymentRequest.requestSentAtEpochMs
-          }
+				const observation = startStep('payment_consume')
+				try {
+					const paymentRequest = JSON.parse(inputMessage.content.toString()) as PaymentRequestMessage
+					const paymentConfirmationId = uuidv4()
+					const outputMessage: PaymentResponseMessage = {
+						paymentRequestId: paymentRequest.paymentRequestId,
+						paymentConfirmationId,
+						requestSentAtEpochMs: paymentRequest.requestSentAtEpochMs
+					}
 
-          console.log('\n\n [x] Received payment request %s', paymentRequest.paymentRequestId)
+					console.log('\n\n [x] Received payment request %s', paymentRequest.paymentRequestId)
 
-          channel.sendToQueue(queuePaymentResponse, Buffer.from(JSON.stringify(outputMessage)))
-          console.log(' [x] Sent payment response %s', JSON.stringify(outputMessage))
-          observation.stop('success')
-        } catch (error) {
-          observation.stop('error')
-          console.error(' [x] Could not process payment request', error)
-        }
-      },
-      {
-        noAck: true
-      }
-    )
-  })
+					channel.sendToQueue(queuePaymentResponse, Buffer.from(JSON.stringify(outputMessage)))
+					console.log(' [x] Sent payment response %s', JSON.stringify(outputMessage))
+					observation.stop('success')
+				} catch (error) {
+					observation.stop('error')
+					console.error(' [x] Could not process payment request', error)
+				}
+			},
+			{
+				noAck: true
+			}
+		)
+	})
 })
 
 ////////////////////////////////////
@@ -115,23 +121,23 @@ amqp.connect('amqp://rabbitmq', function (error0: Error | null, connection: any)
 const app = express()
 
 app.listen(3000, () => {
-  console.log('HTTP Server running on port 3000')
+	console.log('HTTP Server running on port 3000')
 })
 
 app.get('/ticket', (req: any, res: any) => {
-  const observation = startStep('ticket_http')
-  try {
-    const ticketId = uuidv4()
-    console.log('\n\n [x] Create Ticket %s', ticketId)
-    res.json({ ticketId })
-    observation.stop('success')
-  } catch (error) {
-    observation.stop('error')
-    throw error
-  }
+	const observation = startStep('ticket_http')
+	try {
+		const ticketId = uuidv4()
+		console.log('\n\n [x] Create Ticket %s', ticketId)
+		res.json({ ticketId })
+		observation.stop('success')
+	} catch (error) {
+		observation.stop('error')
+		throw error
+	}
 })
 
 app.get('/metrics', async (_req: any, res: any) => {
-  res.set('Content-Type', metricsContentType())
-  res.end(await renderMetrics())
+	res.set('Content-Type', metricsContentType())
+	res.end(await renderMetrics())
 })
